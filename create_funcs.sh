1adac04aaf6bcf1a707ea32cf6142c94c084e1c5
Ember Crooks <ember.crooks@gmail.com>
2022-01-31 19:09:49 -0700

    Adding day 4

--- create_funcs.sh
+++ create_funcs.sh
@@ -3,3 +3,4 @@ su - db2inst1 -c "db2 connect to aocdb; db2 -tvf /database/sql_aoc/day02_part1_s
 su - db2inst1 -c "db2 connect to aocdb; db2 -td@ -vf /database/sql_aoc/day02_part2_sp.sql |tee day02_part2_sp.sql.out; db2 connect reset"
 su - db2inst1 -c "db2 connect to aocdb; db2 -tvf /database/sql_aoc/day03_part1_setup.sql |tee day03_part1_setup.sql.out; db2 connect reset"
 su - db2inst1 -c "db2 connect to aocdb; db2 -td@ -vf /database/sql_aoc/day03_part2.sql |tee day03_part2.sql.out; db2 connect reset"
+su - db2inst1 -c "db2 connect to aocdb; db2 -td@ -vf /database/sql_aoc/day04.sql |tee day04.sql.out; db2 connect reset"
